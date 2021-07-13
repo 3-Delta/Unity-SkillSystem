c6f73783fba1884d1a23e55e02d50a7157a72375
kaclok <kaclok@users.noreply.github.com>
2021-07-13 18:15:07 +0800

    EventEmitter 增加一个内嵌类

--- Assets/EGamePlay/Combat/EventEmitter.cs
+++ Assets/EGamePlay/Combat/EventEmitter.cs
@@ -2,98 +2,117 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class EventEmitter<TEnum> where TEnum : Enum {
-    private Dictionary<TEnum, List<Delegate>> _delegates = new Dictionary<TEnum, List<Delegate>>(0);
+namespace Framework {
+    public class EventHandler {
+        protected readonly List<Delegate> ls = new List<Delegate>(0);
+        public Type actionType => ls.Count > 0 ? ls[0]?.GetType() : null;
+        public int Count => ls.Count;
 
-    protected void Add(TEnum eventType, Delegate action) {
-        if (action != null) {
-            if (!_delegates.TryGetValue(eventType, out List<Delegate> ls)) {
-                ls = new List<Delegate>(1) {action};
-                _delegates.Add(eventType, ls);
-                return;
-            }
-
-            if (action.GetType() != ls[0].GetType()) {
-                ls.Add(action);
-            }
-        }
-    }
-
-    protected void Remove(TEnum eventType, Delegate action) {
-        if (action != null) {
-            if (_delegates.TryGetValue(eventType, out List<Delegate> ls) && ls.Count > 0) {
-                if (action.GetType() == ls[0].GetType()) {
-                    ls.Remove(action);
+        public void Register(Delegate action, bool toReigster) {
+            if (action != null) {
+                if (toReigster) {
+                    if (Count <= 0 || (actionType == action.GetType())) {
+                        ls.Add(action);
+                    }
+                }
+                else {
+                    if (Count > 0 && (actionType == action.GetType())) {
+                        ls.Remove(action);
+                    }
                 }
             }
         }
-    }
 
-    public void Handle(TEnum eventType, Action action, bool toReigster) {
-        if (toReigster) {
-            Add(eventType, action);
-        }
-        else {
-            Remove(eventType, action);
-        }
-    }
-
-    public void Handle<TArg1>(TEnum eventType, Action<TArg1> action, bool toReigster) {
-        if (toReigster) {
-            Add(eventType, action);
-        }
-        else {
-            Remove(eventType, action);
-        }
-    }
-
-    public void Handle<TArg1, TArg2>(TEnum eventType, Action<TArg1, TArg2> action, bool toReigster) {
-        if (toReigster) {
-            Add(eventType, action);
-        }
-        else {
-            Remove(eventType, action);
-        }
-    }
-
-    public void Handle<TArg1, TArg2, TArg3>(TEnum eventType, Action<TArg1, TArg2, TArg3> action, bool toReigster) {
-        if (toReigster) {
-            Add(eventType, action);
-        }
-        else {
-            Remove(eventType, action);
-        }
-    }
-
-    public void Fire(TEnum eventType) {
-        if (_delegates.TryGetValue(eventType, out var ls)) {
+        public void Invoke() {
             for (int i = ls.Count - 1; i >= 0; i--) {
                 (ls[i] as Action)?.Invoke();
             }
         }
-    }
 
-    public void Fire<TArg1>(TEnum eventType, TArg1 arg1) {
-        if (_delegates.TryGetValue(eventType, out var ls)) {
+        public void Invoke<TArg1>(TArg1 arg1) {
             for (int i = ls.Count - 1; i >= 0; i--) {
                 (ls[i] as Action<TArg1>)?.Invoke(arg1);
             }
         }
-    }
 
-    public void Fire<TArg1, TArg2>(TEnum eventType, TArg1 arg1, TArg2 arg2) {
-        if (_delegates.TryGetValue(eventType, out var ls)) {
+        public void Invoke<TArg1, TArg2>(TArg1 arg1, TArg2 arg2) {
             for (int i = ls.Count - 1; i >= 0; i--) {
                 (ls[i] as Action<TArg1, TArg2>)?.Invoke(arg1, arg2);
             }
         }
-    }
 
-    public void Fire<TArg1, TArg2, TArg3>(TEnum eventType, TArg1 arg1, TArg2 arg2, TArg3 arg3) {
-        if (_delegates.TryGetValue(eventType, out var ls)) {
+        public void Invoke<TArg1, TArg2, TArg3>(TArg1 arg1, TArg2 arg2, TArg3 arg3) {
             for (int i = ls.Count - 1; i >= 0; i--) {
                 (ls[i] as Action<TArg1, TArg2, TArg3>)?.Invoke(arg1, arg2, arg3);
             }
+        }
+
+        public void Clear() {
+            ls.Clear();
+        }
+    }
+
+    public class EventEmitter<TEnum> where TEnum : Enum {
+        private readonly Dictionary<TEnum, EventHandler> _delegates = new Dictionary<TEnum, EventHandler>(0);
+
+        private void _Register(TEnum eventType, Delegate action, bool toReigster) {
+            if (toReigster) {
+                if (!_delegates.TryGetValue(eventType, out var handler)) {
+                    handler = new EventHandler();
+                    _delegates.Add(eventType, handler);
+                }
+
+                handler.Register(action, true);
+            }
+            else {
+                if (_delegates.TryGetValue(eventType, out var handler)) {
+                    handler.Register(action, false);
+                }
+            }
+        }
+
+        public void Register(TEnum eventType, Action action, bool toReigster) {
+            _Register(eventType, action, toReigster);
+        }
+
+        public void Register<TArg1>(TEnum eventType, Action<TArg1> action, bool toReigster) {
+            _Register(eventType, action, toReigster);
+        }
+
+        public void Register<TArg1, TArg2>(TEnum eventType, Action<TArg1, TArg2> action, bool toReigster) {
+            _Register(eventType, action, toReigster);
+        }
+
+        public void Register<TArg1, TArg2, TArg3>(TEnum eventType, Action<TArg1, TArg2, TArg3> action, bool toReigster) {
+            _Register(eventType, action, toReigster);
+        }
+
+        public void Invoke(TEnum eventType) {
+            if (_delegates.TryGetValue(eventType, out var handler)) {
+                handler.Invoke();
+            }
+        }
+
+        public void Invoke<TArg1>(TEnum eventType, TArg1 arg1) {
+            if (_delegates.TryGetValue(eventType, out var handler)) {
+                handler?.Invoke(arg1);
+            }
+        }
+
+        public void Invoke<TArg1, TArg2>(TEnum eventType, TArg1 arg1, TArg2 arg2) {
+            if (_delegates.TryGetValue(eventType, out var handler)) {
+                handler?.Invoke(arg1, arg2);
+            }
+        }
+
+        public void Invoke<TArg1, TArg2, TArg3>(TEnum eventType, TArg1 arg1, TArg2 arg2, TArg3 arg3) {
+            if (_delegates.TryGetValue(eventType, out var handler)) {
+                handler?.Invoke(arg1, arg2, arg3);
+            }
+        }
+
+        public void Clear() {
+            _delegates.Clear();
         }
     }
 }
